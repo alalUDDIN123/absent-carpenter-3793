812a9419dbabfbcdca3717027d2213b65337f47e
hemuu12 <harisingh.bisht2001@gmail.com>
2023-01-18 21:40:45 +0530

    day-2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 
 import './App.css';
 import MainRoutes from './Components/MainRoutes';
-import SingleProduct from './Pages/SinglePage/SingleProduct';
+
 
 
 function App() {
   return (
     <>
-    
-      <SingleProduct />
+    <MainRoutes />
+     
     </>
   );
 }
