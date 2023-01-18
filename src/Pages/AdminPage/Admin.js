8314938a442998dd86ca9de1b22bfbc6ff5cb358
Alal Uddin <alalu75666@gmail.com>
2023-01-18 08:18:14 +0530

    Day-2 Start

--- src/Pages/AdminPage/Admin.js
+++ src/Pages/AdminPage/Admin.js
@@ -1,16 +1,35 @@
 import React from 'react'
+import style from "./admin.module.css"
+
 
 function Admin() {
   return (
-    <div>
-      <div>
-        Left Side start
+    <>
+      <div className={style["main-wrapper"]} >
+        <div className={style["container-wrapper"]}>
+
+          {/* Left Side Bar */}
+          <div>
+             <div>Logged User</div>
+             <div>Dashboard</div>
+             <div>Users</div>
+             <div>Orders</div>
+             <div>Products</div>
+             <div>Add Product</div>
+             <div>Logout</div>
+          </div>
+           
+           {/* Right Side bar */}
+          <div>
+            Right Side
+          </div>
+
+        </div>
       </div>
-      <div>
-        Right Side
-      </div>
-    </div>
+    </>
   )
 }
 
 export default Admin
+
+// fp04_371_day_2
