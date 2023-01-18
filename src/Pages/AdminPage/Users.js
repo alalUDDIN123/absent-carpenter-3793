712d66f30b9e1acaa538d2ad83459daafddc5b37
Alal Uddin <alalu75666@gmail.com>
2023-01-18 18:39:51 +0530

    80% User Page Completed

--- src/Pages/AdminPage/Users.js
+++ src/Pages/AdminPage/Users.js
@@ -3,88 +3,91 @@ import LeftSide from './LeftSide'
 import style from "./admin.module.css"
 import userstyle from "./user.module.css"
 
-
 function Users() {
-
-  const [usersData, setUsersData] = useState([]);
-
+    const [userData, setUserData] = useState([]);
 
 
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/todos?_limit=10").then((res) => {
-      return res.json();
-    }).then((resp) => {
-      setUsersData(resp);
-    }).catch((err) => {
-      console.log(err.message);
-    })
-  }, [])
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/todos?_limit=7").then((res) => {
+            return res.json();
+        }).then((resp) => {
+            setUserData(resp)
+        }).catch((err) => {
+            console.log(err.message);
+        })
+    }, [])
 
 
-  return (
-    <>
-      <div className={style["main-wrapper"]} >
-        <div className={style["container-wrapper"]}>
 
-          <LeftSide />
+    return (
+        <>
+            <div className={style["main-wrapper"]} >
+                <div className={style["container-wrapper"]}>
 
-          {/* Right Side bar */}
-          <div>
-            {/* Top Bar */}
-            <div className={userstyle['top-search']} >
-              <div>
-                <input placeholder="Search user" className={userstyle.input} />
+                    <LeftSide />
 
-              </div>
-              <div>
-                <button>Add User +</button>
-              </div>
+                    {/* Right Side bar */}
+                    <div>
+                        <div className={userstyle["main-userTable"]}>
+                            <div className="card">
 
-            </div> <br /> <br /> <br /> <br /> <br />
+                                <div className={userstyle['top-search']} >
+                                    <div>
+                                        <input placeholder="Search user" className={userstyle.input} />
 
-            {/* User table */}
+                                    </div>
+                                    <div>
+                                        <button>Add User +</button>
+                                    </div>
 
-            <div className={userstyle.table} >
-              <table>
-                <thead>
-                  <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Phone</th>
-                    <th>Action</th>
-                  </tr>
-                </thead>
+                                </div>
 
-                <tbody>
+                                <div className="card-body">
 
-                  {usersData &&
-                    usersData.map(item => (
-                      <tr key={item.id}>
-                        <td>{item.id}</td>
-                        <td>user1</td>
-                        <td>user1@gmail.com</td>
-                        <td>Phone4564356394865</td>
-                        <td>
-                          <a  className="btn btn-success">Edit</a>
-                          <a  className="btn btn-danger">Remove</a>
-                          <a  className="btn btn-primary">Details</a>
-                        </td>
-                      </tr>
-                    ))
-                  }
+                                    <table className="table table-bordered">
+                                        <thead className="bg-dark text-white">
+                                            <tr>
+                                                <td>ID</td>
+                                                <td>Name</td>
+                                                <td>Email</td>
+                                                <td>Phone</td>
+                                                <td>Action</td>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
 
-                </tbody>
+                                            {userData &&
+                                                userData.map(item => (
+                                                    <tr key={item.id}>
+                                                        <td>{item.id}</td>
+                                                        <td>alal</td>
+                                                        <td>alal@gmail.com</td>
+                                                        <td>+91 43254359874385</td>
+                                                        <td>
+                                                            <a className="btn btn-success">Edit</a>
+                                                            <a className="btn btn-danger">Remove</a>
+                                                            <a className="btn btn-primary">Details</a>
+                                                        </td>
+                                                    </tr>
+                                                ))
+                                            }
 
-              </table>
+                                        </tbody>
+                                    </table>
+                                    <div className={userstyle["pagination"]} >
+                                        <button>Prev</button>
+                                        <button>1</button>
+                                        <button>Next</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
             </div>
-
-          </div>
-
-        </div>
-      </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Users
