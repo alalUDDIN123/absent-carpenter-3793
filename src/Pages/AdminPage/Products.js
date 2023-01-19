b69dd648933ee5cf5a94116cf8a2b6fc3d4f4196
Alal Uddin <alalu75666@gmail.com>
2023-01-19 10:58:30 +0530

    Day-3 Orders and products page design

--- src/Pages/AdminPage/Products.js
+++ src/Pages/AdminPage/Products.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import LeftSide from './LeftSide'
 import style from "./admin.module.css"
 import CommonTop from './CommonTop'
+import orderStyle from "./orderstyle.module.css"
+
+import Table from './Table'
 
 const sendToCommonTop=[
   {
@@ -19,6 +22,21 @@ const sendToCommonTop=[
 ]
 
 function Products() {
+
+  const [orderData, setOrderData] = useState([]);
+
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos?_limit=6").then((res) => {
+      return res.json();
+    }).then((resp) => {
+      setOrderData(resp)
+    }).catch((err) => {
+      console.log(err.message);
+    })
+  }, [])
+
+
   return (
     <>
     <div className={style["main-wrapper"]} >
@@ -29,6 +47,42 @@ function Products() {
           {/* Right Side bar */}
           <div>
              <CommonTop sendToCommonTop={sendToCommonTop}/>
+
+             <div className={orderStyle["middle-order"]} >
+              <div>
+                <div className="card-body">
+
+                  <table className="table table-bordered">
+                    <thead className="bg-dark text-white">
+                      <tr>
+                        <td>Product ID</td>
+                        <td>Product Title</td>
+                        <td>Price</td>
+                        <td>Action</td>
+                      </tr>
+                    </thead>
+                    <tbody className={orderStyle["order-details"]} >
+
+                      {orderData &&
+                        orderData.map(item => (
+                          <Table  {...item} key={item.id} />
+                        ))
+                      }
+
+                    </tbody>
+                  </table>
+                  <div className={orderStyle["pagination"]} >
+                    <button>Prev</button>
+                    <button>1</button>
+                    <button>Next</button>
+                  </div>
+
+                </div>
+              </div>
+              <div>
+
+              </div>
+            </div>
           </div>
 
         </div>
